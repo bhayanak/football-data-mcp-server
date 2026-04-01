7b61075fc99678b3321a4f1f85b956b3b8e7cd42
bhayanak <ajayyadavmca@gmail.com>
2026-04-01 12:37:41 +0530

    ignored map and minified js

--- packages/football-data-vscode-extension/build.cjs
+++ packages/football-data-vscode-extension/build.cjs
@@ -11,8 +11,8 @@ Promise.all([
     format: 'cjs',
     platform: 'node',
     target: 'node18',
-    sourcemap: true,
-    minify: false,
+    sourcemap: false,
+    minify: true,
   }),
   // Bundle the standalone MCP server from sibling package (runs as child process)
   esbuild.build({
@@ -22,8 +22,8 @@ Promise.all([
     format: 'cjs',
     platform: 'node',
     target: 'node18',
-    sourcemap: true,
-    minify: false,
+    sourcemap: false,
+    minify: true,
   }),
 ])
   .then(() => console.log('Extension built successfully'))
